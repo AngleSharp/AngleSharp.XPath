c056e90ca92fbd8f64104db27fdad15339236a94
Denis Ivanov <ghost181991@gmail.com>
2018-01-05 14:37:50 +0300

    Add SelectNodes method

--- src/AngleSharp.XPath/Extensions.cs
+++ src/AngleSharp.XPath/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using AngleSharp.Dom;
 using AngleSharp.Dom.Html;
 using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Xml;
 using System.Xml.XPath;
@@ -22,7 +24,7 @@ namespace AngleSharp.XPath
 		[DebuggerStepThrough]
 		public static string GetOrAdd(this XmlNameTable table, string array)
 		{
-			string s = table.Get(array);
+			var s = table.Get(array);
 
 			if (s == null)
 			{
@@ -54,6 +56,39 @@ namespace AngleSharp.XPath
 
 			var node = (HtmlDocumentNavigator)it.Current;
 			return node.CurrentNode;
+		}
+
+		/// <summary>
+		/// Selects a list of nodes matching the <see cref="XPath"/> expression.
+		/// </summary>
+		/// <param name="element"></param>
+		/// <param name="xpath">The XPath expression.</param>
+		/// <returns>List of nodes matching <paramref name="xpath"/> query.</returns>
+		/// <exception cref="ArgumentNullException">Throws if <paramref name="element"/> or <paramref name="xpath"/> is <c>null</c></exception>
+		public static List<INode> SelectNodes(this IElement element, string xpath)
+		{
+			if (element == null)
+			{
+				throw new ArgumentNullException(nameof(element));
+			}
+
+			if (xpath == null)
+			{
+				throw new ArgumentNullException(nameof(xpath));
+			}
+			
+			var nav = new HtmlDocumentNavigator(element.Owner, element);
+			var it = nav.Select(xpath);
+			var result = new List<INode>();
+
+			while (it.MoveNext())
+			{
+				var naviagtor = (HtmlDocumentNavigator) it.Current;
+				var e = naviagtor.CurrentNode;
+				result.Add(e);
+			}
+
+			return result;
 		}
 	}
 }
