1e1c55d5e18bd7313487439e4b0889902766e3e4
Denis Ivanov <divanov@sanatorium-is.ru>
2019-12-29 15:01:40 +0400

    #18 Fix but without namespaces.

--- src/AngleSharp.XPath/Extensions.cs
+++ src/AngleSharp.XPath/Extensions.cs
@@ -1,40 +1,34 @@
+using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.XPath;
+
 namespace AngleSharp.XPath
 {
-    using AngleSharp.Dom;
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Xml;
-    using System.Xml.XPath;
-
     /// <summary>
     /// Hosts the extension methods for XPath parsing.
     /// </summary>
     public static class Extensions
-	{
+    {
         /// <summary>
         /// Creates a new navigator for the given document.
         /// </summary>
         /// <param name="document">The document to extend.</param>
         /// <returns>The navigator for XPath expressions.</returns>
-		public static XPathNavigator CreateNavigator(this IDocument document)
-		{
+        public static XPathNavigator CreateNavigator(this IDocument document)
+        {
             var doc = document ?? throw new ArgumentNullException(nameof(document));
             return new HtmlDocumentNavigator(doc, doc.DocumentElement);
-		}
+        }
 
-		[DebuggerStepThrough]
-		internal static String GetOrAdd(this XmlNameTable table, String array)
-		{
-			var s = table.Get(array);
-
-			if (s == null)
-			{
-				return table.Add(array);
-			}
-
-			return s;
-		}
+        [DebuggerStepThrough]
+        internal static string GetOrAdd(this XmlNameTable table, string array)
+        {
+            var s = table.Get(array);
+            return s ?? table.Add(array);
+        }
 
         /// <summary>
         /// Selects a single node (or returns null) matching the <see cref="XPath"/> expression.
@@ -43,21 +37,14 @@ namespace AngleSharp.XPath
         /// <param name="xpath">The XPath expression.</param>
         /// <returns>The node matching <paramref name="xpath"/> query, if any.</returns>
         /// <exception cref="ArgumentNullException">Throws if <paramref name="element"/> or <paramref name="xpath"/> is <c>null</c></exception>
-        public static INode SelectSingleNode(this IElement element, String xpath)
-		{
+        public static INode SelectSingleNode(this IElement element, string xpath)
+        {
             var el = element ?? throw new ArgumentNullException(nameof(element));
             var xp = xpath ?? throw new ArgumentNullException(nameof(xpath));
-			var nav = new HtmlDocumentNavigator(el.Owner, el);
-			var it = nav.Select(xp);
-
-			if (it.MoveNext())
-            {
-                var node = (HtmlDocumentNavigator)it.Current;
-                return node.CurrentNode;
-            }
-
-            return null;
-		}
+            var nav = new HtmlDocumentNavigator(el.Owner, el);
+            var it = nav.SelectSingleNode(xp);
+            return ((HtmlDocumentNavigator) it)?.CurrentNode;
+        }
 
         /// <summary>
         /// Selects a list of nodes matching the <see cref="XPath"/> expression.
@@ -66,22 +53,24 @@ namespace AngleSharp.XPath
         /// <param name="xpath">The XPath expression.</param>
         /// <returns>List of nodes matching <paramref name="xpath"/> query.</returns>
         /// <exception cref="ArgumentNullException">Throws if <paramref name="element"/> or <paramref name="xpath"/> is <c>null</c></exception>
-        public static List<INode> SelectNodes(this IElement element, String xpath)
+        public static List<INode> SelectNodes(this IElement element, string xpath)
         {
             var el = element ?? throw new ArgumentNullException(nameof(element));
             var xp = xpath ?? throw new ArgumentNullException(nameof(xpath));
             var nav = new HtmlDocumentNavigator(el.Owner, el);
-			var it = nav.Select(xp);
-			var result = new List<INode>();
+            var it = nav.Select(xp);
+            var result = new List<INode>();
 
-			while (it.MoveNext())
-			{
-				var naviagtor = (HtmlDocumentNavigator) it.Current;
-				var e = naviagtor.CurrentNode;
-				result.Add(e);
-			}
+            while (it.MoveNext())
+            {
+                // ReSharper disable once IdentifierTypo
+                var naviagtor = (HtmlDocumentNavigator) it.Current;
+                // ReSharper disable once PossibleNullReferenceException
+                var e = naviagtor.CurrentNode;
+                result.Add(e);
+            }
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
